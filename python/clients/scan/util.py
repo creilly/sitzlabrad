56dd0cb393eb18e9c77bbcdbd07b0c0c9fec2843
chris reilly <reilly.christopher@gmail.com>
2015-10-13 23:00:47 -0500

    added utility functions for converting unicode json to ascii and converting input/output arguments to python keywords

---
+++ python/clients/scan/util.py
@@ -0,0 +1,15 @@
+def mangle(d):
+    return {
+        key.replace(' ','_'):value 
+        for key, value in d.items()
+    }
+
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key):byteify(value) for key,value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
