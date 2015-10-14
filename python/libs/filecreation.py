3793b3843f411f3badbcfc0498c0619f36dfb6bc
chris reilly <reilly.christopher@gmail.com>
2015-10-13 23:01:36 -0500

    added standardized time string to filecreation functions

--- python/libs/filecreation.py
+++ python/libs/filecreation.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 POOH_DATA_PATH = 'z:/data/pooh/'
 
+def get_datetime():
+    datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 def get_filename(folders=None,description=None,extension='.dat'):
     if folders is None: 
         folders = []
