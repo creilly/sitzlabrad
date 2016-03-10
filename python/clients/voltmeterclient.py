f98da55f381a5d0e616c056b14eb42c074acc33c
Christopher Reilly <reilly.christopher@gmail.com>
2016-03-10 03:34:21 -0600

    got rid of needless callback stuff for voltmeter client

--- python/clients/voltmeterclient.py
+++ python/clients/voltmeterclient.py
@@ -1,42 +1,8 @@
 from twisted.internet.defer import inlineCallbacks, returnValue
 ON_ACTIVE_CHANNELS_CHANGED = 210
 ON_SAMPLING_DURATION_CHANGED = 211
+
 class VoltmeterClient:
-    def __init__(self,vm_server):
-        self.vm_server = vm_server
-        self.on_active_channels_changed_cb = None
-        vm_server.on_active_channels_changed(
-            ON_ACTIVE_CHANNELS_CHANGED
-        )
-        vm_server.addListener(
-            listener=self._on_active_channels_changed,
-            source=None,
-            ID=ON_ACTIVE_CHANNELS_CHANGED
-        )
-        self.on_sampling_duration_changed_cb = None
-        vm_server.on_sampling_duration_changed(
-            ON_SAMPLING_DURATION_CHANGED
-        )
-        vm_server.addListener(
-            listener=self._on_sampling_duration_changed,
-            source=None,
-            ID=ON_SAMPLING_DURATION_CHANGED
-        )
-    def _on_active_channels_changed(self,c,msg):
-        cb = self.on_active_channels_changed_cb
-        if cb is not None: cb(msg)
-
-    def on_active_channels_changed(self,cb):
-        self.on_active_channels_changed_cb = cb
-
-    def _on_sampling_duration_changed(self,c,msg):
-        cb = self.on_sampling_duration_changed_cb
-        if cb is not None: cb(msg)
-
-    def on_sampling_duration_changed(self,cb):
-        self.on_sampling_duration_changed_cb = cb
-
-class ChannelClient:
     def __init__(self,vm_server,channel):
         self.vm_server = vm_server
         self.channel = channel
