ba5c98e54700a19376252067efd78d6cda6ed092
Christopher Reilly <reilly.christopher@gmail.com>
2016-04-28 09:01:00 -0500

    cleaned up messaging sign up for sm client

--- python/clients/steppermotorclient.py
+++ python/clients/steppermotorclient.py
@@ -1,26 +1,20 @@
-ON_NEW_POSITION = 210
-ON_BUSY_STATUS_CHANGED = 211
-ON_ENABLED_STATUS_CHANGED = 212
 class StepperMotorClient:
     def __init__(self,sm_name,sm_server):
         self.sm_name = sm_name
         self.sm_server = sm_server
+        sm_server.on_new_position.connect(
+            self._on_new_position
+        )
+        sm_server.on_busy_status_changed.connect(
+            self._on_busy_status_changed
+        )
+        sm_server.on_enabled_status_changed.connect(
+            self._on_enabled_status_changed
+        )
         self.on_new_position_cb = None
         self.on_busy_status_changed_cb = None
         self.on_enabled_status_changed_cb = None
-        sm_server.on_new_position(ON_NEW_POSITION)
-        sm_server.addListener(
-            listener=self._on_new_position,source=None,ID=ON_NEW_POSITION
-        )
-        sm_server.on_busy_status_changed(ON_BUSY_STATUS_CHANGED)
-        sm_server.addListener(
-            listener=self._on_busy_status_changed,source=None,ID=ON_BUSY_STATUS_CHANGED
-        )
-        sm_server.on_enabled_status_changed(ON_ENABLED_STATUS_CHANGED)
-        sm_server.addListener(
-            listener=self._on_enabled_status_changed,source=None,ID=ON_ENABLED_STATUS_CHANGED
-        )
-
+        
     def _on_new_position(self,c,msg):
         cb = self.on_new_position_cb
         if cb is not None and msg[0] == self.sm_name:
