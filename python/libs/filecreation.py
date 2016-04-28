fcc6c2812bec9915296dda4b3e4ed5a46e9264c6
chris reilly <reilly.christopher@gmail.com>
2016-04-28 11:28:55 -0500

    adapted filecreation to my directory

--- python/libs/filecreation.py
+++ python/libs/filecreation.py
@@ -2,16 +2,16 @@ import os
 from os import path
 from datetime import datetime
 
-POOH_DATA_PATH = 'z:/data/pooh/'
+MOLECULE_DATA_PATH = './'
 
 def get_datetime():
     return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-def get_filename(folders=None,description=None,extension='.dat'):
+def get_filename(directory=None,folders=None,description=None,extension='.dat'):
     if folders is None: 
         folders = []
     filepath = path.join(
-        POOH_DATA_PATH,
+        MOLECULE_DATA_PATH if directory is None else directory,
         datetime.now().strftime("%Y-%m-%d"),
         *folders
     )
@@ -20,17 +20,18 @@ def get_filename(folders=None,description=None,extension='.dat'):
     time = datetime.now().strftime("%H%M%S")
     return os.path.join(
         filepath,
-        (
-            '_'.join(
-                (
-                    time,
-                    description,
-                    extension
-                ) if description is not None else (
-                    time,
-                    extension
-                )
-            ) 
+        '.'.join(
+            (
+                '_'.join(
+                    (
+                        time,
+                        description,
+                    ) if description is not None else (
+                        time,
+                    )
+                ),
+                extension
+            )
         )
     )
 def get_file_dialog():
